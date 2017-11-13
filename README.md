7d414fee37b20b068336c167af043365239f429d
Zovin <zovin.khanmohammed@tylertech.com>
2017-11-13 10:01:36 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cautious-octo-fiesta
 Test Repository for the court team
+
+* This project is a test for the incode Court team to see if we like github
---
+++ TestApp/TestApp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _output;
+        public string Output {
+            get => _output;
+            set => _output = value;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Output = "helloworld";
+        }
+
+        private void chkButton_Click(object sender, RoutedEventArgs e) => MessageBox.Show($"Output is {Output}");
+    }
+}
